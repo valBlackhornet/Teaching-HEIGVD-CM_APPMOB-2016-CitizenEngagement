f316142e2ed710452a4343aa412c60ade5fd7725
valeriestauffer <valerie.stauffer@heig-vd.ch>
2016-04-05 15:18:51 +0200

    api modifié et 2-3 design dans templates

--- www/js/issue.js
+++ www/js/issue.js
@@ -11,8 +11,30 @@ angular.module('citizen-engagement.issue', [])
     };
 
     $scope.newIssue = function() {
+
+    // // Take the long lati
+    //   $http({
+    //     method: 'GET',
+    //     url: //trouver comment avoir la position //apiUrl + '/users/logister',
+    //     data: //$scope.user
+    //   }).success(function(user) {
+
+    //     // If successful, write lat et long
+
+    //     // Go to the issue creation tab.
+    //     $state.go('tab.issues');
+
+    //   }).error(function() {
+    //     $scope.error = 'Could not have the lat and long';
+    //   });
+
+
         // Go to the issue creation tab.
         $state.go('tab.issues/newIssue');
     };
+
+
+
   })
 ;
+ 
